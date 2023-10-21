cce88d9c3695bdb2d21286f5811b76dea8e2ea4f
JGalvez1776 <64766167+JGalvez1776@users.noreply.github.com>
2023-10-21 11:38:56 -0700

    Added diff functionality

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def main():
             inputs: list[str] = args.input
             verbose: bool = args.verbose or args.crash
             crash: bool = args.crash
+            diff: bool = args.diff
             name: Optional[str] = args.name
             results: list[tuple[dict[str, Any], bool, TestResult]]
-            results = run(inputs, verbose, crash, name)
+            results = run(inputs, verbose, crash, name, diff)
             out: list[dict[str, str]] = [
-                {"name": te["file"], "passed": str(passed)}
-                for te, passed, _ in results
+                {"name": te["file"], "passed": str(passed)} for te, passed, _ in results
             ]
             pprint.pprint(out, indent=4)
         case _:
@@ -36,43 +36,28 @@ def parse_args():
 
     dump = subparsers.add_parser("dump", help="Dump test pickles")
     dump.add_argument("input", nargs="+", help="input test pickle file(s)")
-    dump.add_argument(
-        "--name", type=str, default=None, help="limit to a specific test"
-    )
+    dump.add_argument("--name", type=str, default=None, help="limit to a specific test")
 
     create = subparsers.add_parser("create", help="create a test pickle")
     create.add_argument("input", type=str, nargs="+", help="input file(s)")
-    create.add_argument(
-        "--output", type=str, required=True, help="output file"
-    )
-    create.add_argument(
-        "--function", type=str, required=True, help="function to test"
-    )
+    create.add_argument("--output", type=str, required=True, help="output file")
+    create.add_argument("--function", type=str, required=True, help="function to test")
     create.add_argument(
         "--compare", type=str, required=True, help="comparison function"
     )
-    create.add_argument(
-        "--verbose", action="store_true", help="verbose output"
-    )
+    create.add_argument("--verbose", action="store_true", help="verbose output")
 
     format = create.add_mutually_exclusive_group(required=True)
-    format.add_argument(
-        "--pickle", action="store_true", help="input is a pickle"
-    )
+    format.add_argument("--pickle", action="store_true", help="input is a pickle")
     format.add_argument("--json", action="store_true", help="input is a json")
-    format.add_argument(
-        "--text", action="store_true", help="input is a text file"
-    )
+    format.add_argument("--text", action="store_true", help="input is a text file")
 
     run = subparsers.add_parser("run", help="run tests in a pickle file")
     run.add_argument("input", nargs="+", help="input test pickle file(s)")
-    run.add_argument(
-        "--verbose", action="store_true", help="enable verbose output"
-    )
+    run.add_argument("--verbose", action="store_true", help="enable verbose output")
     run.add_argument("--crash", action="store_true", help="crash on error")
-    run.add_argument(
-        "--name", type=str, default=None, help="limit to a specific test"
-    )
+    run.add_argument("--name", type=str, default=None, help="limit to a specific test")
+    run.add_argument("--diff", action="store_true", help="show diff on failure")
 
     return parser.parse_args()
 
